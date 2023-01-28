9d45e0cad383aa6ca885ea0dd849f4ba2b4f1424
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-01-28 22:14:27 +0530

    First Way for JSONModel

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,6 +4,34 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("GithubSAP-UI5-Fiori-3_JSONModel.controller.View1", {
+		
+		onInit : function(){
+			//<<--------------------------First Way--------------------------------->>
+			//First Way to use JSONModel.
+				//Step 1)Declare a brand new model.
+				var oModel = new sap.ui.model.json.JSONModel();
+				//Step 2)Set a Data to the model.
+				oModel.setData({
+					"student":{
+						"name":"Kurvesh",
+						"no":20012012037,
+						"sem":8
+					}
+				});
+				//Step 3)Set the model to App / View / Controll level.
+				this.getView().setModel(oModel);
+		
+			
+			//<<---------------------------Second Way--------------------------------->>
+			//Second Way to use JSONModel.
+				//Step 1)Declare the brand new model.
+				
+				//Step 2)Set the data to the model.
+				//Step 3)Set the model to the App / View / Controll Level.
+			
+		}
+		
+		
 
 	});
 });
